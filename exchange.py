e9216e5f5d21fea7f3b1ccf3cf76384185212582
Anshul Chauhan <anshulchauhan@outlook.com>
2017-08-05 18:45:40 +0530

    Add exception handling for wrong/unavailable currency is given

--- exchange.py
+++ exchange.py
@@ -13,18 +13,22 @@ __version__ = "0.1"
 __email__ = "anshulchauhan@outlook.com"
 
 @click.command()
-@click.option('--curr_from', '-f', default = 'USD', type = str, help = 'This the currency you are trying to convert from.')
-@click.option('--curr_to', '-t', default = 'INR', type = str, help = 'This is the currency you\'re converting to.')
-@click.option('--value', '-v', default = 1, type = float, help = 'The amount you want to convert.')
+@click.option('--curr_from', '-f', default='USD', type=str,
+              help='This the currency you are trying to convert from.')
+@click.option('--curr_to', '-t', default='INR', type=str,
+              help='This is the currency you\'re converting to.')
+@click.option('--value', '-v', default=1, type=float, help='The amount you want to convert.')
 
-def cli(curr_from, curr_to, value):
+def cli(curr_from, curr_to, value: float):
+    """main function being called by Click"""
 
-    if curr_from == curr_to:
-        click.echo("1.00")
-
+    #if curr_from == curr_to:
+    #    click.echo("1.00")
     main_api = 'http://api.fixer.io/latest?'
-
     url = main_api + urllib.parse.urlencode({'base':curr_from})
 
-    json_data = requests.get(url).json()
-    click.echo(json_data['rates'][curr_to] * value)
+    try:
+        json_data = requests.get(url).json()
+        click.echo(json_data['rates'][curr_to] * value)
+    except KeyError:
+        click.echo("Unavailable currency")
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup (
-    name="CurrencyConversionCli",
+    name="ExchangeRates",
     version='0.1',
     py_modeules=['exchange'],
     install_requires=[
