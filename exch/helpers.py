75228e50a94b1e555b5396780b0ba04878505c93
Anshul <anshulchauhan@outlook.com>
2017-08-23 16:58:28 +0530

    refactor fixer function with try/except block

--- exch/helpers.py
+++ exch/helpers.py
@@ -2,19 +2,23 @@
 
 from urllib.parse import urlencode
 import requests
-from decimal import Decimal, getcontext
 
-getcontext().prec = 2
-
-def fixer(base, target, value=1, date='latest'):
+def fixer(base, target, value, date='latest'):
     """get currency exchange rate from fixer.io in JSON"""
-    
+
     main_api = 'http://api.fixer.io/{}?'.format(date)
     url = main_api + urlencode({'base':base})
     json_data = requests.get(url).json()
 
-    return round(json_data['rates'][target] * value, 2)
+    try:
+        result = round(json_data['rates'][target] * value, 2)
+    except requests.exceptions.ConnectionError:
+        result = "Connection Error"
+    except KeyError:
+        result = 1.00 if (base == target) else "Invalid currency"
 
-def google_finance_converter(base, target, value):
-    """ parse the Google Finance Converter html to extract the value"""
-    pass
+    return result
+
+#def google_finance_converter(base, target, value):
+#    """ parse the Google Finance Converter html to extract the value"""
+#    pass
