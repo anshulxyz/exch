26a3023230f81381119ea9b6330f27ef149e109c
Anshul <anshulchauhan@outlook.com>
2017-08-26 17:17:32 +0530

    change the dpendency on urllib to support py2.7
    Since the urllib has been split into different modules for python3,
    it was creating an issue while support for both the version. Instad,
    the requests library is universal.

--- exch/helpers.py
+++ exch/helpers.py
@@ -1,16 +1,15 @@
 """ helper functions to gather the currency rates """
 
-from urllib.parse import urlencode
 import requests
 
 def fixer(base, target, value, date='latest'):
     """get currency exchange rate from fixer.io in JSON"""
 
     main_api = 'http://api.fixer.io/{}?'.format(date)
-    url = main_api + urlencode({'base':base})
+    url = requests.get(main_api, params={'base':base})
 
     try:
-        json_data = requests.get(url).json()
+        json_data = requests.get(url.url).json()
         result = round(json_data['rates'][target] * value, 2)
     except requests.exceptions.ConnectionError:
         result = "Connection Error"
